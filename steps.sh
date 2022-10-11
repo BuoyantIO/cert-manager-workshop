e0b915b270efadbf5e8d638f8bd174af23bfb258
jasonmorgan <jmorgan@f9vs.com>
2022-10-11 08:17:04 -0400

    Ready to add cert-manager.
    * Added demo-magic
    * Added commands from policy workshop

---
+++ steps.sh
@@ -0,0 +1,157 @@
+#!/bin/env bash
+source ../demo-magic.sh
+clear
+
+## Cluster setup
+k3d cluster delete workshop > /dev/null 2>&1 || true
+k3d cluster create workshop > /dev/null 2>&1
+
+## Load up Booksapp
+# curl -sL run.linkerd.io/emojivoto.yml | kubectl apply -f -
+kubectl create ns booksapp && \
+  curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/booksapp.yml \
+  | kubectl -n booksapp apply -f -
+
+clear
+
+#All installs to be done with helm
+## cert-manager install
+
+# Install cert-manager
+# create certs for Linkerd
+
+## Linkerd
+
+# Install CRDS
+
+
+
+## Inject booksapp
+pe "kubectl get deploy -n booksapp -o yaml | linkerd inject - | kubectl apply -f -"
+wait 
+clear
+
+## Look around
+#Things mostly work
+pe "linkerd viz stat deploy -n booksapp"
+wait 
+clear
+
+
+# No effective policies
+# pe "linkerd viz authz -n booksapp deployment"
+# wait 
+# clear
+
+## Harden our ns
+### Default deny
+### Configure a deny policy for booksapp
+pe 'kubectl annotate ns booksapp config.linkerd.io/default-inbound-policy=deny'
+wait
+clear
+
+pe 'kubectl get pods -n booksapp'
+wait
+clear
+
+# pe "linkerd viz authz -n booksapp deployment"
+# wait 
+# clear
+
+pe "linkerd viz stat deploy -n booksapp"
+wait 
+clear
+
+# Traffic is still there
+## Apps still restart thanks to default exemptions for health checks
+pe 'kubectl rollout restart -n booksapp deploy'
+wait
+clear
+
+# Now traffic is gone
+## Alternately watch the traffic
+# pe "linkerd viz authz -n booksapp deployment"
+# wait 
+# clear
+
+# pe "linkerd viz stat deploy -n booksapp"
+# wait 
+# clear
+
+### Allow admin traffic
+pe "kubectl apply -f manifests/booksapp/admin_server.yaml"
+wait 
+clear
+
+pe "kubectl apply -f manifests/booksapp/allow_viz.yaml"
+wait 
+clear
+
+pe "bat -l yaml manifests/booksapp/admin_server.yaml"
+wait 
+clear
+
+pe "bat -l yaml manifests/booksapp/allow_viz.yaml"
+wait 
+clear
+
+### Allow app traffic
+pe "kubectl apply -f manifests/booksapp/authors_server.yaml"
+wait 
+clear
+
+pe "kubectl apply -f manifests/booksapp/books_server.yaml"
+wait 
+clear
+
+pe "kubectl apply -f manifests/booksapp/webapp_server.yaml"
+wait 
+clear
+
+pe "kubectl apply -f manifests/booksapp/allow_namespace.yaml"
+wait 
+clear
+
+pe "bat -l yaml manifests/booksapp/authors_server.yaml"
+wait 
+clear
+
+pe "bat -l yaml manifests/booksapp/allow_namespace.yaml "
+wait 
+clear
+
+### No Traffic app? no ports!
+
+# HTTPRoutes, Locking down who can do what with our books
+## switch from watching traffic to watching pods
+pe "kubectl apply -f manifests/booksapp/authors_get_route.yaml"
+wait 
+clear
+## wait a minute for authors to become unready
+
+## App should become unready
+
+pe 'kubectl apply -f manifests/booksapp/authors_get_policy.yaml'
+wait
+clear
+
+## Lets fix our busted health checks, no more default routes
+
+pe 'kubectl apply -f manifests/booksapp/authors_probe.yaml'
+wait
+clear
+## wait a minute for authors to become ready
+### Check readiness
+
+## Check app
+
+### Looks good
+### Can't update books
+
+pe 'kubectl apply -f manifests/booksapp/authors_modify_route.yaml'
+wait
+clear
+
+pe 'kubectl apply -f manifests/booksapp/authors_modify_policy.yaml'
+wait
+clear
